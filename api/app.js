e046090b832490c75072f13217120d172d83df11
HypnosKernel <tquantity.com@gmail.com>
2026-07-02 17:39:36 -0400

    Use shared Express handler for Vercel

--- api/app.js
+++ api/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const { createApp } = require('../src/app');
-
 // Vercel imports this module for each serverless invocation. Do not call
 // app.listen() or start background timers here; local/traditional hosting still
 // uses server.js for that behavior.
-module.exports = createApp();
+module.exports = require('../src/app');
